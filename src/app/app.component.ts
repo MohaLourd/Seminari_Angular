3a8ace2aa7f091dda1e6466358be7b48ddf1d931
MohaLourd <mohamed.lourdjane@estudiantat.upc.edu>
2025-03-18 17:01:35 +0100

    Seminari de Angular, Registre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UsuarioComponent } from "./usuario/usuario.component";
-import { LoginComponent } from "./login/login.component";
+import { UsuarioComponent } from './usuario/usuario.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UsuarioComponent, LoginComponent],
+  imports: [RouterOutlet, UsuarioComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  standalone: true
-  
+  standalone: true,
 })
 export class AppComponent {
   title = 'angular-seminari6';
   loggedin: boolean = false;
-  getLoggedIn(loggedin: boolean){
+  registeredIn: boolean = false;
+  getLoggedIn(loggedin: boolean) {
     this.loggedin = loggedin;
+  }
+  getRegisteredIn(registeredIn: boolean) {
+    this.registeredIn = registeredIn;
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,13 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = "https://reqres.in/api/login";
-  constructor(private http: HttpClient) { }
-  
+  private apiUrl = 'https://reqres.in/api/login';
+  constructor(private http: HttpClient) {}
+
   login(credentials: { email: string; password: string }): Observable<any> {
     return this.http.post(this.apiUrl, credentials);
+  }
+  private apiUrlRegister = 'http://localhost:4000/api/users/register2';
+
+  registerUser(credentials: {
+    email: string;
+    age: string;
+    name: string;
+  }): Observable<any> {
+    return this.http.post(this.apiUrlRegister, credentials);
   }
 }
